98df04a18033d1f6c8688d93ec79a66d2994b080
tomxi <tom.xi@nyu.edu>
2023-05-02 10:54:14 -0400

    made progress

--- ssdm/salami.py
+++ ssdm/salami.py
@@ -34,7 +34,7 @@ class Track:
         if self._sr != sr:
             self._y, self._sr = librosa.load(self.audio_path, sr=sr)
         return self._y, self._sr
-    
+   
     
     def jam(
         self, 
@@ -43,14 +43,13 @@ class Track:
             self._jam = jams.load(os.path.join(self.salami_dir, f'jams/{self.tid}.jams'))
         return self._jam
 
-    
+
     def num_annos(
         self,
     ) -> int:
         return len(self.jam().search(namespace='segment_salami_upper'))
-    
-    
-    
+
+
     def ts(
         self
     ) -> np.array:
@@ -129,11 +128,11 @@ class Track:
         return dmat
 
 
-def segmentation_annotation(
+    def segmentation_annotation(
         self,
         mode: str = 'normal', # {'normal', 'expand'},
         anno_id: int = 0,
-    ) -> list: 
+    ) -> jams.JAMS: 
         """
         A list of `jams.Annotation`s with two modes: {'normal', 'expand'}
         """
@@ -141,17 +140,17 @@ def segmentation_annotation(
         lower_annos = self.jam().search(namespace='segment_salami_lower')
         hier_annos = []
         if mode == 'normal':
-            return [upper_annos[anno_id], lower_annos[anno_id]]
+            return jams.JAMS(annotations=[upper_annos[anno_id], lower_annos[anno_id]])
         elif mode == 'expand':
             upper_expanded = ssdm.expand_hierarchy(upper_annos[anno_id])
             lower_expanded = ssdm.expand_hierarchy(lower_annos[anno_id])
-            return  upper_expanded + lower_expanded
+            return  jams.JAMS(annotations=upper_expanded + lower_expanded)
 
 
     def segmentation_lsd(
         self,
         config = None,
-    ) -> list:
+    ) -> jams.JAMS:
         """
         A list of `jams.Annotation`s segmented with config
         """
@@ -168,7 +167,6 @@ def segmentation_annotation(
 
         # create jams annotation
         hier_anno = []
-
         for layer_bdry_idx, layer_labels in zip(est_bdry_idxs, est_sgmt_labels):
             anno = jams.Annotation(namespace='segment_open')
             for i, segment_label in enumerate(layer_labels):
@@ -180,62 +178,37 @@ def segmentation_annotation(
                     value=segment_label
                 )
             hier_anno.append(anno)
-        return hier_anno
+
+        lsd_jam = jams.JAMS(annotations=hier_anno, sandbox=config)
+        return lsd_jam
 
 
     def segmentation_adobe(
         self,
-    ) -> list:
-        # TODO
-        pass
+    ) -> jams.JAMS:
+        result_dir = '/scratch/qx244/data/ISMIR21-Segmentations/SALAMI/def_mu_0.1_gamma_0.1/'
+        filename = f'{self.tid}.mp3.msdclasscsnmagic.json'
+
+        with open(os.path.join(result_dir, filename), 'rb') as f:
+            justin_result = json.load(f)
+
+        # Build jams
+        hier_anno = []
+        for layer, (bdry, labels) in enumerate(justin_result):
+            ann = jams.Annotation(namespace='segment_open')
+            for ival, label in zip(bdry, labels):
+                ann.append(time=ival[0], duration=ival[1]-ival[0], value=label)
+            hier_anno.append(ann)
+            
+        justin_jam = jams.JAMS(annotations=hier_anno, sandbox={'mu': 0.1, 'gamma': 0.1})
+        return justin_jam
 
 
 ### Stand alone functions
-def compute_tau(
-    sdm, 
-    segmentation,
-    ts, 
-    region: str = 'full', #{'full', 'path'}
-    quantize: bool = True,
-) -> float:
-    """
-    """
-    pass
-
-
-def compute_l(
-    proposal, 
-    annotation,
-) -> float:
-    """
-    """
-    pass
-
-
-def segmentation_to_meet(
-    segmentation, 
-    ts,
-) -> np.array:
-    """
-    """
-    pass
-
-
-def segmentation_to_mireval(
-    segmentation
-) -> tuple:
-    """
-    """
-    pass
-
-    hier_intervals, hier_values = ([], [])
-    return hier_intervals, hier_values
-
-
 def get_ids(
     split: str = 'dev',
     id_paths: str = '/home/qx244/scanning-ssm/revive/split_ids.json',
-    out_type: str = 'list' # {'set', 'list'}
+    out_type: str = 'list' # one of {'set', 'list'}
 ) -> list:
     """ split can be ['audio', 'jams', 'excluded', 'new_val', 'new_test', 'new_train']
         Dicts sotred in id_paths json file.
@@ -253,13 +226,75 @@ def get_ids(
     else:
         print('invalid out_type')
         return None
+
+
+def segmentation_to_meet(
+    segmentation, 
+    ts,
+) -> np.array:
+    """
+    """
+    # initialize a 3d array to store all the meet matrices for each layer
+    hier_anno = segmentation.annotations
+    n_level = len(hier_anno)
+    n_frames = len(ts)
+    meet_mat_per_level = np.zeros((n_level, n_frames, n_frames))
+
+    # put meet mat of each level of hierarchy in axis=0
+    for level in range(n_level):
+        layer_anno = hier_anno[level]
+        label_samples = layer_anno.to_samples(ts)
+        le = preprocessing.LabelEncoder()
+        encoded_labels = le.fit_transform([l[0] if len(l) > 0 else 'NL' for l in label_samples])
+        meet_mat_per_level[level] = np.equal.outer(encoded_labels, encoded_labels).astype('float') * (level + 1)
+
+    # get the deepest level matched
+    return np.max(meet_mat_per_level, axis=0)
+
+
+def segmentation_to_mireval(
+    segmentation
+) -> tuple:
+    """
+    """
+    mir_eval_segments = []
+    for anno in segmentation.annotations:
+        mir_eval_segments.append(
+            anno.to_interval_values()
+        )
+    return mir_eval_segments
+
+
+def compute_tau(
+    sdm: np.array, 
+    segmentation: jams.JAMS,
+    ts: np.array, 
+    region: str = 'full', #{'full', 'path'}
+    quantize: bool = True, 
+) -> float:
+    """
+    """
+    meet_mat = segmentation_to_meet(segmentation, ts)
+    if quantize:
+        #TODO
+        meet_mat = np.eye(len(ts))
+
+    if region == 'full':
+        return -stats.kendalltau(sdm.flatten(), meet_mat.flatten())[0]
+    elif region == 'path':
+        meet_diag = np.diag(meet_mat, k=1)
+        sdm_diag = np.diag(sdm, k=1)
+        return -stats.kendalltau(meet_diag, sdm_diag)[0]
+    else:
+        raise librosa.ParameterError('region can only be "full" or "path"')
     
 
-# Optional do last.
-def mask_sdm(
-    sdm, 
-    options
-) -> np.ndarray:
+
+def compute_l(
+    proposal, 
+    annotation,
+) -> float:
     """
     """
     pass
+

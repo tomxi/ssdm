589ac0f3adb0ce0d106b614b61bbf5b8a5f24cf6
tomxi <tom.xi@nyu.edu>
2023-05-17 13:58:03 -0400

    changed import order

--- ssdm/__init__.py
+++ ssdm/__init__.py
@@ -1,8 +1,9 @@
+from .salami import *
+from .utils import *
+
 from . import feature
 from . import scluster
 from . import scanner
 from . import viz
 
-from .salami import *
-from .utils import *
 from .expand_hier import expand_hierarchy
